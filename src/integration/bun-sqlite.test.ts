1278d0d075e218abd5cc76245b594d75613575b4
Soner Sayakci <s.sayakci@gmail.com>
2024-09-02 16:54:01 +0200

    feat: add a bun sqlite repository

---
+++ src/integration/bun-sqlite.test.ts
@@ -0,0 +1,33 @@
+import { describe, expect, test } from "bun:test";
+import { BunSqliteRepository } from "./bun-sqlite.js";
+import { SimpleShop } from "../repository.js";
+
+describe("Bun SQLite", async () => {
+	test("createShop", async () => {
+		const repo = new BunSqliteRepository(":memory:");
+
+		await repo.createShop("test", "test", "test");
+
+		const shop = await repo.getShopById("test");
+
+		expect(shop).not.toBeNull();
+
+		expect(shop?.getShopId()).toBe("test");
+		expect(shop?.getShopUrl()).toBe("test");
+		expect(shop?.getShopSecret()).toBe("test");
+
+		await repo.deleteShop("test");
+
+		expect(repo.getShopById("test")).resolves.toBeNull();
+
+		await repo.createShop("test", "test", "test");
+
+		expect(repo.getShopById("test")).resolves.not.toBeNull();
+
+		await repo.updateShop(new SimpleShop("test", "foo.com", "test"));
+
+		const updatedShop = await repo.getShopById("test");
+
+		expect(updatedShop?.getShopUrl()).toBe("foo.com");
+	});
+});
