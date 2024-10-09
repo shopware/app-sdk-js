50ad2d8146cb65704a7b5a65a9df35b5cdfc7f29
Soner Sayakci <s.sayakci@shopware.com>
2024-10-09 13:46:26 +0200

    feat: update cloudflare example

--- examples/cloudflare-hono/src/index.ts
+++ examples/cloudflare-hono/src/index.ts
@@ -1,14 +1,20 @@
 import { configureAppServer } from "@shopware-ag/app-server-sdk/integration/hono";
 import { CloudflareShopRepository } from "@shopware-ag/app-server-sdk/integration/cloudflare-kv";
 import { Hono } from "hono";
+import { logger } from 'hono/logger'
 import type {
   AppServer,
   Context,
   ShopInterface,
+  SimpleShop,
 } from "@shopware-ag/app-server-sdk";
 import { createNotificationResponse } from "@shopware-ag/app-server-sdk/helper/app-actions";
+import { EntityRepository } from "@shopware-ag/app-server-sdk/helper/admin-api";
+import type { ActionButtonRequest } from "@shopware-ag/app-server-sdk/types";
+import { Criteria } from "@shopware-ag/app-server-sdk/helper/criteria";
 
 const app = new Hono();
+app.use(logger());
 
 declare module "hono" {
   interface ContextVariableMap {
@@ -18,17 +24,33 @@ declare module "hono" {
   }
 }
 
-app.post('/app/action-button/product', async ctx => {
-  console.log(`Got request from Shop ${ctx.get('shop').getShopId()}`)
-  return createNotificationResponse('success', 'YEAA');
-});
-
 configureAppServer(app, {
-  appName: 'SwagTest',
-  appSecret: 'SwagTest',
+  appName: 'Test',
+  appSecret: 'Test',
   shopRepository: (ctx) => {
     return new CloudflareShopRepository(ctx.env.shopStorage);
   }
-})
+});
+
+type Product = {
+  id: string;
+  name: string;
+};
+
+app.post("/app/action-button", async (c) => {
+  const ctx = c.get("context") as Context<SimpleShop, ActionButtonRequest>;
+
+  const repository = new EntityRepository<Product>(ctx.httpClient, "product");
+
+  // get the products clicked by action button
+  const entitySearchResult = await repository.search(new Criteria(ctx.payload.data.ids));
+
+  console.log(entitySearchResult.total);
+  console.log(entitySearchResult.first()?.name)
+
+  await repository.upsert(entitySearchResult.data.map(product => ({ id: product.id, name: 'yippiee' })));
+
+  return createNotificationResponse("success", "Product name updated yeaaa");
+});
 
 export default app;
